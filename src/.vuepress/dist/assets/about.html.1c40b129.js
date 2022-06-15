bf66b52956d37453eca9f876be6b2b60524cd73b
Nikita Starikov <nikita.starikov@gmail.com>
2022-06-15 10:49:15 +0500

    remove built dist

--- src/.vuepress/dist/assets/about.html.1c40b129.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-22a39d25","path":"/about.html","title":"About","lang":"ru-RU","frontmatter":{},"excerpt":"","headers":[],"git":{},"filePathRelative":"about.md"}');export{t as data};
--- src/.vuepress/dist/assets/index.html.7ec0fd78.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-8daa1a0e","path":"/","title":"Index page","lang":"ru-RU","frontmatter":{},"excerpt":"","headers":[],"git":{},"filePathRelative":"index.md"}');export{e as data};
